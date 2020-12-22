25898f5cbeb57e219b96bf95dbb511bab213d3ce
Rohan Lamb <lambrohan@gmail.com>
2020-12-22 17:24:27 +0530

    drawer-complete

--- components/Drawer.vue
+++ components/Drawer.vue
@@ -1,16 +1,22 @@
 <template>
-  <div id="drawer" class="bg-blue-500 rounded-r-lg">
-    <MenuButton @click.native="displayMenu = !displayMenu" />
-    <div
+  <div
+    id="drawer"
+    class="bg-blue-500 rounded-r-lg"
+    :class="[{ expanded: displayMenu }]"
+    @mouseenter="displayMenu = !displayMenu"
+    @mouseleave="displayMenu = false"
+  >
+    <nuxt-link
       v-for="item in menuItem"
       :key="item.name"
-      class="menu flex mb-5 items-center"
+      :to="item.route"
+      class="menu flex py-4 items-center px-5"
     >
-      <img class="w-8" :src="item.icon" alt="" />
+      <img class="w-8" :src="item.icon" />
       <p class="text-white text-lg font-normal menu-title">
         {{ item.name }}
       </p>
-    </div>
+    </nuxt-link>
   </div>
 </template>
 
@@ -20,11 +26,11 @@ export default {
     return {
       displayMenu: false,
       menuItem: [
-        { icon: 'dashboard.svg', name: 'Dashboard' },
-        { icon: 'calender.svg', name: 'Appointment' },
-        { icon: 'users.svg', name: 'Patients' },
-        { icon: 'collection.svg', name: 'Products' },
-        { icon: 'chat.svg', name: 'Notifications' },
+        { icon: 'dashboard.svg', name: 'Dashboard', route: '/' },
+        { icon: 'calender.svg', name: 'Appointment', route: '/appointments' },
+        { icon: 'users.svg', name: 'Patients', route: '/patients' },
+        { icon: 'collection.svg', name: 'Products', route: '/products' },
+        { icon: 'chat.svg', name: 'Notifications', route: '/notifications' },
       ],
     }
   },
@@ -35,7 +41,39 @@ export default {
 <style lang="scss" scoped>
 #drawer {
   position: relative;
-  min-width: 85px;
+  min-width: 0px;
   height: 100vh;
+  transition: 0.3s ease all;
+  overflow: hidden;
+
+  .menu {
+    position: relative;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .menu-title {
+    position: absolute;
+    top: 50%;
+    left: 68px;
+    text-align: left;
+    transform: translateY(-50%);
+    opacity: 0;
+    transition: 0.5s ease all;
+  }
+}
+
+.expanded {
+  min-width: 220px !important;
+
+  .menu-title {
+    opacity: 1 !important;
+  }
+}
+
+.nuxt-link-exact-active {
+  @apply bg-blue-600;
 }
 </style>
