ca33e8ecb7e554aa2c932cea740bc646b12a72e6
Rohan Lamb <lambrohan@gmail.com>
2021-01-27 14:50:12 +0530

    prescription-print

--- components/ConsentFormDialog.vue
+++ components/ConsentFormDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="consent-dialog"
-    class="fiexd top-0 left-0 z-6 w-full h-screen bg-white fixed bg-opacity-70"
+    class="fiexd top-0 left-0 z-6 w-full h-screen bg-white fixed bg-opacity-70 z-10"
   >
     <div class="dialog flex flex-col">
       <div class="tlbr flex bg-gray-100 w-full">
